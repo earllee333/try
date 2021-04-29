009421496345849bcd95e6145c878f1c38a168de
earllee333 <jerrykiet2012@gmail.com>
2021-04-29 18:21:39 +0800

    v1.05

--- functions/guides.js
+++ functions/guides.js
@@ -1,19 +1,19 @@
 exports.handler = async(event,context)=>{
     const guides=[
-        {title:'Blog1'},
-        {author:'Luigi'},
-        {age:28}
+        {title:'Blog1',author:'Luigi',age:18},
+        {title:'Blog2',author:'Mario',age:25},
+        {title:'Blog3',author:'Naween',age:35},
     ]
+    
     if(context.clientContext.user){
-        return{
-        status:200,
+         return{
+        statusCode:200,
         body:JSON.stringify(guides)
      }
     }
     return{
-        status:401,
-        body:JSON.stringify({mssg:'You do not have promision to see this'})
+        statusCode:401,
+        body:JSON.stringify({mssg:'Sorry... You do not have permession to see'})
     }
-
-    
+   
 }
--- pages/guides.js
+++ pages/guides.js
@@ -1,16 +1,50 @@
 import styles from '../styles/Guides.module.css'
-import {useEffect} from 'react'
+import {useContext, useEffect,useState} from 'react'
+import AuthContext from '../stores/authContext'
 export default function Guides() {
-
+  const{user,authReady} = useContext(AuthContext)
+  const[error,setError]=useState(null)
+  const[guides,setGuides]=useState(null)
   useEffect(() => {
-    fetch('/.netlify/functions/guides')
-    .then(res=>res.json())
-    .then((data)=>{console.log(data)})
-  },[])
-
+    if(authReady){
+      fetch('/.netlify/functions/guides', user &&{
+        headers: {
+          Authorization:'Bearer '+ user.token.access_token
+        }//Do rember plus space ' space ' after Bearer
+      })
+      //.then(res=>res.json())
+      .then(res=>{
+        //console.log(res)
+        if(!res.ok){
+          throw Error('You must be logged')
+        }
+        return res.json()
+      })
+      .then((data)=>{
+        setGuides(data)
+        setError(null)
+      })
+      .catch((err) => {
+        setError(err.message)
+        setGuides(null)
+      })
+    }
+    },[user,authReady])
   return (
     <div className={styles.guides}>
-      <h2>All Guides</h2>
+      {!authReady && <div>Loading</div>}
+
+      {error && <div className={styles.error}>
+        <p>{error}</p>
+        </div>}
+      
+      {guides && guides.map(guide=>(
+        <div key ={guide.title} className={styles.guides}>
+          <h3>{guide.title}</h3>
+          <h4>Written by {guide.author}</h4>
+
+        </div>
+      ))}
     </div> 
   )
 }
